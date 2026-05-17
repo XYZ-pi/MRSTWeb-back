eb8a4141772378e2a48e48f278a217bd1ca91f0e
Iulia Nazarova <iulia.nazarova@isa.utm.md>
2026-05-17 13:42:02 +0300

    Auth(3)

--- TKDequipShop.API/Controllers/AuthController.cs
+++ TKDequipShop.API/Controllers/AuthController.cs
@@ -3,27 +3,35 @@ using Microsoft.AspNetCore.Mvc;
 using TKDequipShop.BusinessLogic;
 using TKDequipShop.DataAccess.Context;
 using TKDequipShop.Domains.Models.Auth;
+using TKDequipShop.BusinessLogic.Interfaces;
 
 namespace TKDequipShop.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/session")]
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private IAuthActions _authActions;
 
-        private readonly AppDbContext _db = new AppDbContext();
-        private readonly TokenService _tokenService = new TokenService();
+        public AuthController()
+        {
+            var _bl = new TKDequipShop.BusinessLogic.BusinessLogic();
+            _authActions = _bl.GetAuthActions();
+        }
 
         [HttpPost("auth")]
         public IActionResult Login([FromBody] LoginDto _login)
         {
-            var user = _db.UserDatas.FirstOrDefault(u =>
-                u.UserName == _login.UserName &&
-                u.Password == _login.Password);
+            var token = _authActions.LoginAction(_login);
+            if (token == null) return Unauthorized("Invalid credentials");
+            return Ok(new { token });
+        }
 
-            if (user == null) return Unauthorized("Invalid credentials");
-
-            var token = _tokenService.GenerateToken(user);
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] RegisterDto _register)
+        {
+            var token = _authActions.RegisterAction(_register);
+            if (token == null) return BadRequest("User already exists");
             return Ok(new { token });
         }
     }
